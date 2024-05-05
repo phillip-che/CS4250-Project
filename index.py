0b04aba890b6896d4140ff2c46907d10ad738ef3
adenletchworth <adenrletchworth@gmail.com>
2024-05-05 16:44:41 -0700

    feat(indexer, parser): Add Word2Vec model to the pipeline

--- index.py
+++ index.py
@@ -3,9 +3,8 @@ from collections import defaultdict
 import numpy as np
 import math
 from sentence_transformers import SentenceTransformer
-from nltk.util import ngrams
 from sklearn.feature_extraction.text import TfidfVectorizer
-import spacy
+from gensim.models import Word2Vec
 
 class Indexer:
     def __init__(self, db_name='CPP_PROJECT', db_host='localhost', db_port=27017):
@@ -14,13 +13,23 @@ class Indexer:
         self.documents_collection = self.db.documents
         self.index_collection = self.db.index
         self.doc_count = self.documents_collection.count_documents({})
-        self.nlp = spacy.load('en_core_web_sm')
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.word2vec_model = Word2Vec.load('./models/word2vec.model')
+    
+    def add_documents(self, documents):
+        for doc_id, text in documents.items():
+            self.add_document(text, doc_id)
 
     def add_document(self, text, doc_id):
         text_str = " ".join(text)  
 
-        vector = self.model.encode([text_str])[0]
+        word_vectors = [self.word2vec_model.wv[word] for word in text if word in self.word2vec_model.wv]
+        if word_vectors:
+            aggregated_vector = np.mean(word_vectors, axis=0)
+        else:
+            aggregated_vector = np.zeros(self.word2vec_model.vector_size)
+            
+        word_vector_pretrained = self.model.encode([text_str])[0]
 
         term_frequency = self.get_term_frequency(text)
         tf_idf = self.calculate_tf_idf(term_frequency)
@@ -30,24 +39,13 @@ class Indexer:
                 '$set': {
                     '_id': doc_id,
                     'text': text_str,
-                    'vector': vector.tolist(), 
+                    'word_vector_pretrained': word_vector_pretrained.tolist(), 
+                    'word_vector_word2vec': aggregated_vector.tolist(), 
                     'tf_idf': dict(tf_idf)  
                 }
             }, 
             upsert=True
         )
-
-        grams = self.get_grams(text)
-        entities = self.get_entities(text_str)
-
-        for bigram in grams['bigram']:
-            self.update_inverted_index(self.get_term_frequency([" ".join(bigram)]), doc_id)
-            
-        for trigram in grams['trigram']:
-            self.update_inverted_index(self.get_term_frequency([" ".join(trigram)]), doc_id)
-
-        for entity in entities:
-            self.update_inverted_index(self.get_term_frequency([entity]), doc_id)
 
         self.update_inverted_index(term_frequency, doc_id)
         self.doc_count += 1  
@@ -74,17 +72,6 @@ class Indexer:
             idf = math.log((self.doc_count + 1) / (doc_freq + 1)) if doc_freq else 0 
             tf_idf[token] = tf * idf
         return tf_idf
-    
-    def get_grams(self, text):
-        bigram = list(ngrams(text, 2))
-        trigram = list(ngrams(text, 3))
-        return {'bigram': bigram, 'trigram': trigram}
-
-         
-    def get_entities(self, text):
-        doc = self.nlp(text)
-        return [ent.text for ent in doc.ents]
-
 
     def query_index(self, query_text):
         # THE QUERY TEXT TRANSFORMATION WILL OCCUR ELSEWHERE JUST FOR TESTING
@@ -96,7 +83,7 @@ class Indexer:
 
         results = []
         for doc in self.documents_collection.find():
-            doc_vector = np.array(doc['vector'])
+            doc_vector = np.array(doc['word_vector_pretrained'])
             doc_similarity = np.dot(doc_vector, query_vector) / (np.linalg.norm(doc_vector) * np.linalg.norm(query_vector))
             
             tf_idf_similarity = sum(doc['tf_idf'].get(token, 0) * self.calculate_tf_idf({token: query_term_frequency[token]}).get(token, 0) for token in query_tokens)
@@ -129,6 +116,6 @@ if __name__ == "__main__":
     indexer.add_document(['new', 'document', 'new', 'brand', 'hello', 'for', 'test', 'just', 'just'], 'doc9')
     indexer.add_document(['newest', 'documents', 'document'], 'doc8')
     indexer.add_document(['greetings', 'earth', 'earth', 'new'], 'doc10')
-    indexer.update_all_tfidf() # NEED TO UPDATE TF-IDF AFTER ADDING DOCUMENTS
+    indexer.update_all_tfidf() 
     query_text = "hello world"
     print(indexer.query_index(query_text))
