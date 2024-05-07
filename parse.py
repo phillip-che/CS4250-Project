fc08cf05fa9c65d04d05640b1b03fddd8822571a
adenletchworth <adenrletchworth@gmail.com>
2024-05-07 14:08:40 -0700

    feat(integration): Integrate Parse with Indexer

--- parse.py
+++ parse.py
@@ -17,15 +17,15 @@ class CustomTokenizer:
     
     def tokenize_and_lemmatize_word2vec(self, text):
         sentences = nltk.sent_tokenize(text)
-        return [[self.lemmatizer.lemmatize(token) for token in nltk.word_tokenize(sentence)] for sentence in sentences]
+        return [self.lemmatizer.lemmatize(token) for sentence in sentences for token in nltk.word_tokenize(sentence)]
 
 class Parser:
     def __init__(self, db_name='CPP_PROJECT', db_host='localhost', db_port=27017):
         self.client = MongoClient(host=db_host, port=db_port)
         self.db = self.client[db_name]
         self.pages_col = self.db.pages
-        custom_tokenizer = CustomTokenizer()
-        self.vectorizer = CountVectorizer(lowercase=True, stop_words='english', ngram_range=(1, 5), tokenizer=custom_tokenizer.tokenize_and_lemmatize)
+        self.custom_tokenizer = CustomTokenizer()
+        self.vectorizer = CountVectorizer(lowercase=True, stop_words='english', ngram_range=(1, 5), tokenizer=self.custom_tokenizer.tokenize_and_lemmatize)
 
     def extract(self, soup):
         found = soup.find('div', {"class": "row pgtop"})
@@ -40,7 +40,8 @@ class Parser:
     def process_texts(self):
         documents = []
         doc_ids = []
-        
+        ordered_tokens_per_document = []
+
         # Process all faculty pages
         for html_content in self.pages_col.find({"url": {"$regex": "^https://www.cpp.edu/faculty/"}}):
             if html_content:
@@ -49,14 +50,14 @@ class Parser:
                 extracted_text = self.extract(soup)
                 prepared_text = self.prepare_text(extracted_text)
                 documents.append(prepared_text)
-        
+                ordered_tokens_per_document.append(self.custom_tokenizer.tokenize_and_lemmatize_word2vec(prepared_text))
+
         # Fit the vectorizer on the entire corpus (Gives us the vocabulary)
         self.vectorizer.fit(documents)
         joblib.dump(self.vectorizer, './models/vectorizer.pkl')
         
-        # Fit the word2vec model on the entire corpus
-        custom_tokenizer = CustomTokenizer()
-        word2vec_model = Word2Vec(sentences=custom_tokenizer.tokenize_and_lemmatize_word2vec(" ".join(documents)), vector_size=100, window=5, min_count=1, workers=4)
+        # Fit the word2vec model on the combined tokens from all documents
+        word2vec_model = Word2Vec(sentences=ordered_tokens_per_document, vector_size=100, window=5, min_count=1, workers=4)
         word2vec_model.save('./models/word2vec.model')
 
         # Transform each document and collect tokens and counts (Keeps the order of the documents)
@@ -66,13 +67,12 @@ class Parser:
             feature_names = self.vectorizer.get_feature_names_out()
             document_tokens[doc_id] = dict(zip(feature_names, vector.flatten()))
 
-        return document_tokens
+        # Document tokens is a dictionary where the key is the document id and the value is a dictionary of tokens and their counts
+        # Ordered tokens per document is a list of lists where each list is the tokens of a document
+        return document_tokens, ordered_tokens_per_document
 
 if __name__ == "__main__":
     parser = Parser()
-    tokens_by_document = parser.process_texts()
-    for doc_id, tokens in tokens_by_document.items():
-        print(f"Document ID: {doc_id}")
-        for token, count in tokens.items():
-            if count > 0:
-                print(f"{token}: {count}")
+    tokens_by_document, ordered_tokens_per_document = parser.process_texts()
+    #print(tokens_by_document[list(tokens_by_document.keys())[0]])  # Print tokens of the first document
+    print(ordered_tokens_per_document[0])  
